d8794d98bf04ec9385e9523a92dc0fbb7c7852f4
Guilherme <guilhermersouza34@gmail.com>
2024-08-09 16:29:05 -0300

    Primeiro Ola Mundo em php

---
+++ Ex000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso em Video</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo"Ola Mundo";
+        ?>
+    </h1>
+    <p>Iniciando o curso de php</p>
+</body>
+</html>

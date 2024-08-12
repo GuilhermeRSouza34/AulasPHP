70fa635e5d4ee38d7522e0002097a8db1c892f96
Guilherme <guilhermersouza34@gmail.com>
2024-08-11 22:10:29 -0300

    Sistema de envio para feeedback

--- feedback/index.php
+++ feedback/index.php
@@ -4,8 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema para envio de um feedback</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    
+    <div class="container">
+        <h1>Formulario de Feedback</h1>
+        <form method="post" action="feedback.php">
+            <label for="nome">Nome: </label>
+            <input type="text" name="nome" id="nome" required>
+
+            <label for="comentario">Comentário: </label>
+            <textarea name="comentario" id="comentario" required></textarea>
+
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
 </body>
 </html>

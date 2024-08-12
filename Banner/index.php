56ffaf21278ab529a79314825f9797b8666df2c1
Guilherme <guilhermersouza34@gmail.com>
2024-08-12 19:51:51 -0300

    Criando um plugin para wordpress utilizanndo php para apresentação de um banner

---
+++ Banner/index.php
@@ -0,0 +1,8 @@
+<?php 
+/*
+Plugin Name: Custom Banner
+Description: Adiciona um banner personalizado na parte superior do site.
+Version: 1.0
+Author: Guilherme <guilhermersouza34@gmail.com>
+*/
+?>
